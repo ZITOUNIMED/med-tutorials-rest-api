f46e7e64b457f84a3a6be159facba502fb97d3e5
ZITOUNI Mohamed <zitouni.mohamed.email@gmail.com>
2019-07-11 08:27:18 +0200

    try to solve Unable to access lob stream error with posgres error

--- src/main/java/com/example/demo/web/rest/DocumentController.java
+++ src/main/java/com/example/demo/web/rest/DocumentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,6 +42,7 @@ public class DocumentController {
 	}
 
 
+	@Transactional(readOnly=true)
 	@GetMapping("/{id}")
 	public ResponseEntity<Document> getDocument(@PathVariable Long id){
 		return ResponseEntity.ok(documentService.getDocument(id));
